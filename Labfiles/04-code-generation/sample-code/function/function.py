886a0aa1250bd3418d1dc8f4451b4e6941bedbd7
Gabriel Santos Tramontin <145608943+gabrielstramontin@users.noreply.github.com>
2024-06-08 23:56:48 -0300

    Update function.py

--- Labfiles/04-code-generation/sample-code/function/function.py
+++ Labfiles/04-code-generation/sample-code/function/function.py
@@ -1,4 +1,9 @@
 def absolute_square(num1, num2):
+    # Calculate the absolute difference between num1 and num2
     result = abs(num1 - num2)
+
+    # Square the result:
     result *= result
+
+    # Return the squared result:
     return result
