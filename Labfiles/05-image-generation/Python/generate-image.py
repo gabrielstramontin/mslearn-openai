eddb36e8c78fa430d6ea6f61ef4c1eb5257671ba
Gabriel Santos Tramontin <145608943+gabrielstramontin@users.noreply.github.com>
2024-06-09 00:06:43 -0300

    Update generate-image.py

--- Labfiles/05-image-generation/Python/generate-image.py
+++ Labfiles/05-image-generation/Python/generate-image.py
@@ -3,19 +3,18 @@ import time
 import os
 from dotenv import load_dotenv
 
-def main(): 
-        
+def main():
     try:
-        # Get Azure OpenAI Service settings
+        # Get Azure OpenAI Service Settings:
         load_dotenv()
         api_base = os.getenv("AZURE_OAI_ENDPOINT")
         api_key = os.getenv("AZURE_OAI_KEY")
         api_version = '2023-06-01-preview'
         
-        # Get prompt for image to be generated
+        # Get prompt for image to be generated:
         prompt = input("\nEnter a prompt to request an image: ")
 
-        # Make the initial call to start the job
+        # Make the initial call to start the job:
         url = "{}openai/images/generations:submit?api-version={}".format(api_base, api_version)
         headers= { "api-key": api_key, "Content-Type": "application/json" }
         body = {
@@ -25,27 +24,30 @@ def main():
         }
         submission = requests.post(url, headers=headers, json=body)
 
-        # Get the operation-location URL for the callback
+        # Get the operation-location URL for the callback:
         operation_location = submission.headers['Operation-Location']
 
-        # Poll the callback URL until the job has succeeeded
+        # Poll the callback URL until the job has succeeeded:
         status = ""
         while (status != "succeeded"):
             time.sleep(3) # wait 3 seconds to avoid rate limit
             response = requests.get(operation_location, headers=headers)
             status = response.json()['status']
 
-        # Get the results
+        # Get the results:
         image_url = response.json()['result']['data'][0]['url']
 
-        # Display the URL for the generated image
+        # Display the URL for the generated image:
         print(image_url)
         
-
     except Exception as ex:
         print(ex)
 
 if __name__ == '__main__': 
     main()
 
-
+'''
+At the end, in the integrated terminal, enter the appropriate command to run and test the application:
+[pip install requests
+python generate-image.py]
+'''
